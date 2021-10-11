e19f7e0824b5f37ef9f83762a98cac06f56634e0
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-10-11 09:47:15 +0800

    Migrated .html to .php
    All .html pages required are now .php file.
    
    Navigations in this files are now provided by navigation.php so we only need to change 1 file when adding list items in navigation bar.

--- admin/admin-page-room-editRoom.php
+++ admin/admin-page-room-editRoom.php
@@ -1,3 +1,9 @@
+<?php
+
+require_once("customFiles/php/directories/directories.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -52,148 +58,7 @@
     </nav>
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-      <!-- Brand Logo -->
-      <a href="./" class="brand-link text-center">
-        <span class="brand-text font-weight-light">Hotel Reservation System</span>
-      </a>
-
-      <!-- Sidebar -->
-      <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-            <!-- Dashboard -->
-            <li class="nav-item menu-close">
-              <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-tachometer-alt"></i>
-                <p>
-                  Dashboard
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="./" class="nav-link">
-                    <i class="fas fa-receipt nav-icon"></i>
-                    <p>Reservations</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="analytics" class="nav-link">
-                    <i class="fas fa-chart-pie nav-icon"></i>
-                    <p>Web Analytics</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-
-            <!-- Pages -->
-            <li class="nav-item menu-open">
-              <a href="#" class="nav-link active">
-                <i class="nav-icon fas fa-file"></i>
-                <p>
-                  Pages
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="rooms" class="nav-link active">
-                    <i class="fas fa-door-closed nav-icon"></i>
-                    <p>Rooms</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="amenities" class="nav-link">
-                    <i class="fas fa-car nav-icon"></i>
-                    <p>Amenities</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-
-            <!-- Accounts -->
-            <li class="nav-item">
-              <a href="accounts" class="nav-link">
-                <i class="nav-icon fas fa-users"></i>
-                <p>
-                  Accounts
-                </p>
-              </a>
-            </li>
-
-            <!-- Vouchers -->
-            <li class="nav-item">
-              <a href="vouchers" class="nav-link">
-                <i class="nav-icon fas fa-tags"></i>
-                <p>
-                  Vouchers
-                </p>
-              </a>
-            </li>
-
-            <!-- Billing -->
-            <li class="nav-item">
-              <a href="billing" class="nav-link">
-                <i class="nav-icon fas fa-money-bill"></i>
-                <p>
-                  Billing
-                </p>
-              </a>
-            </li>
-
-            <!-- Settings -->
-            <li class="nav-item menu-close">
-              <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-cogs"></i>
-                <p>
-                  Settings
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="appearance" class="nav-link">
-                    <i class="fas fa-image nav-icon"></i>
-                    <p>Appearance</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="webpage" class="nav-link">
-                    <i class="fas fa-file nav-icon"></i>
-                    <p>Webpage</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="profile" class="nav-link">
-                    <i class="fas fa-user nav-icon"></i>
-                    <p>My Account</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-
-            <!-- Logout -->
-            <li class="nav-item">
-              <a href="login" class="nav-link">
-                <i class="nav-icon fas fa-sign-out-alt"></i>
-                <p>
-                  Logout
-                </p>
-              </a>
-            </li>
-
-          </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
-      </div>
-      <!-- /.sidebar -->
-    </aside>
+    <?php include __navigation__;?>
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
 
@@ -1148,7 +1013,7 @@
     $(document).ready(function () {
       lastSavedRoomImage =
         `/public_assets/rooms/${xyzroominfocba.basicInfo.roomTypeID}/${xyzroominfocba.basicInfo.roomTypeID}-cover.jpg`;
-      $(".roomImageCard > img").attr('src', lastSavedRoomImage);
+      $(".roomImageCard > img").attr('src', lastSavedRoomImage + "?t="+ +new Date());
       queries["roomTypeID"] = xyzroominfocba.basicInfo.roomTypeID;
       $("[data-roomdescription]").val(xyzroominfocba.basicInfo.desc);
       $("#roomName").html(xyzroominfocba.basicInfo.name);
@@ -1312,6 +1177,18 @@ toggleLoadingScreen();
       },
     }
   </script>
+
+<!-- Script to toggle navigation buttons -->
+<script>
+  let activeNav = document.querySelector(".sidebar > nav > ul > li:nth-child(2)"); //change :nth(n) value
+  if (activeNav.querySelector('ul') != null){
+    activeNav.className += " menu-is-opening menu-open";
+    activeNav.querySelector('.menu-open > ul > li:nth-child(1) > a').classList.toggle('active'); //change :nth(n) value
+    activeNav.querySelector('ul').style.display = "block";
+  }
+  activeNav.querySelector('a:nth-child(1)').classList.toggle('active'); //do not change this
+</script>
+
 </body>
 
 </html>
