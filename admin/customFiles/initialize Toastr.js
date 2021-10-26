6da717afc3fcbf013d0459c637b0ac811d6b49a6
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-10-26 20:23:25 +0800

    stye: change toast title class

--- admin/customFiles/initialize Toastr.js
+++ admin/customFiles/initialize Toastr.js
@@ -2,5 +2,8 @@ var Toast = Swal.mixin({
     toast: true,
     position: 'top',
     showConfirmButton: false,
-    timer: 3500
+    timer: 3500,
+    customClass: {
+        title: "ml-3"
+    }
 });
