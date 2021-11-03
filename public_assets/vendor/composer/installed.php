f78a7e0e196c059cba4f642e35dcd3336fd26e6b
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-03 19:15:46 +0800

    build: add firebase-jwt package

---
+++ public_assets/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '368470f45ef6a039301fbe9ff25515798cad324b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '368470f45ef6a039301fbe9ff25515798cad324b',
+    ),
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v5.4.0',
+      'version' => '5.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd2113d9b2e0e349796e72d2a63cf9319100382d2',
+    ),
+  ),
+);
