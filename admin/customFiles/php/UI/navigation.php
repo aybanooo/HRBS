041d96a2b8dc433f85fcc4fef2a8e175ac0dd240
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-04 21:21:49 +0800

    refractor: update logout href

--- admin/customFiles/php/UI/navigation.php
+++ admin/customFiles/php/UI/navigation.php
@@ -183,7 +183,7 @@ LOGOUT
 
         <!-- Logout -->
         <li class="nav-item">
-          <a href="login" class="nav-link">
+          <a href="logout" class="nav-link">
             <i class="nav-icon fas fa-sign-out-alt"></i>
             <p>
               Logout
