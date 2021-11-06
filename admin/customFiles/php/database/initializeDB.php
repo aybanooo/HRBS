65fe7a5007c05b656fd6a27155b08d26fa3e557e
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-06 14:06:59 +0800

    dev: add getConnError function
    a function that identifies if developer mode is on then return error

--- admin/customFiles/php/database/initializeDB.php
+++ admin/customFiles/php/database/initializeDB.php
@@ -8,11 +8,16 @@ require_once(__F_OUTPUT_HANDLER__);
 $ini = array_merge(parse_ini_file(__CONF_DB__), parse_ini_file(__CONF_PRIVATE__));
 mysqli_report(MYSQLI_REPORT_STRICT);
 
+// A function that identifies if developer mode is on then return the connection error
+function getConnError($conn) {
+    return __CONF_DMODE_PARSED__ ?  mysqli_error($conn) : "" ;
+}
+
 // Create connection
 try {
 $conn = new mysqli($ini['DB_SERVERNAME'], $ini['DB_USERNAME'], $ini['DB_PASS'], $ini['DB_NAME']);
 } catch (mysqli_sql_exception $e) {
-    $output->setFailed("Cannot connect to database.");
+    $output->setFailed("Cannot connect to database.", __CONF_DMODE_PARSED__ ? $e : null);
     echo $output->getOutput(true);
     die();
 }
