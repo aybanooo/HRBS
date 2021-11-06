259d5e15f4fc11c0bf4a1d6effaf74f7146e7fe2
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-06 15:42:42 +0800

    fix: formatting and checking of amenity id

--- admin/customFiles/php/database/amenitiesControls/removeAmenity.php
+++ admin/customFiles/php/database/amenitiesControls/removeAmenity.php
@@ -8,12 +8,20 @@ $id = tonotwtf($_POST['amid'], 3);
 
 prepareForSQL($conn, $id, 2);
 
-if(empty($id))
+$id = intval($id);
+
+if(mysqli_num_rows($resultID = mysqli_query($conn, "SELECT `amenityID` FROM `amenities` WHERE `amenityID`=$id LIMIT 1;")) == 1)
+    $id = mysqli_fetch_all($resultID, MYSQLI_NUM)[0][0];
+else
+    die($output->setFailed('Amenity doesn\'t exist'));
+#var_dump($id);
+
+if($id==="")
     echo $output->setFailed('Cannot find amenity');
 
 if(mysqli_query($conn, "DELETE FROM `amenities` WHERE `amenityID`=$id LIMIT 1")) {
-    unlink(__D_AMENITIES__."/$id/image.jpeg");
-    rmdir(__D_AMENITIES__."/$id/");
+    unlink(__D_AMENITIES__."$id/image.jpeg");
+    rmdir(__D_AMENITIES__."$id/");
     die($output->setSuccessful('Deleted'));
 }
 else
