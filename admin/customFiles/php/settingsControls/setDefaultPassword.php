965d9b7c478ec454c3fc715ecc160a8291c24430
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-06 12:46:36 +0800

    dev: add auto create defPass in setting if missing

--- admin/customFiles/php/settingsControls/setDefaultPassword.php
+++ admin/customFiles/php/settingsControls/setDefaultPassword.php
@@ -4,6 +4,7 @@ require_once(dirname(__FILE__, 2)."/directories/directories.php");
 require_once __initDB__;
 require_once __F_FORMAT__;
 require_once __F_VALIDATIONS__;
+require_once __F_DB_HANDLER__;
 
 if(!isPassFormat($_POST['inp-defPass']))
     echo $output->setFailed(testPass($_POST['inp-defPass']));
@@ -13,6 +14,8 @@ $defVal = password_hash($_POST['inp-defPass'], PASSWORD_DEFAULT);
 $wtfdPass = towtf($_POST['inp-defPass'], 5);
 
 prepareForSQL($conn, $wtfdPass);
+
+createSettingIfMissing("defPass", $wtfdPass, 3);
 
 if(mysqli_query($conn, "ALTER TABLE `empaccountdetails` ALTER COLUMN `password` SET DEFAULT '$defVal';") && mysqli_query($conn, "UPDATE `settings` SET `value`='$wtfdPass' WHERE `name` like 'defPass' LIMIT 1;")) {
     echo $output->setSuccessful("Default password have been updated");
