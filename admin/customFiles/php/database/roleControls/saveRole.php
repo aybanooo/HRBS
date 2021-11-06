1de50f4f1e537082dab32f252cc7bbb7d83e49b6
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-06 21:57:28 +0800

    dev: implement role save validation

--- admin/customFiles/php/database/roleControls/saveRole.php
+++ admin/customFiles/php/database/roleControls/saveRole.php
@@ -3,6 +3,7 @@ require_once(dirname(__FILE__, 3)."/directories/directories.php");
 require_once(__initDB__);
 require_once __F_VALIDATIONS__;
 require_once __F_FORMAT__;
+require_once __F_DB_HANDLER__;
 
 #echo json_encode($_POST);
 
@@ -28,13 +29,41 @@ try {
     die();
 }
 
+#print_r($_POST['perms']);
+
+#$sql = 'SELECT employee.empID, employee.accessID,COUNT(permissions.permId) FROM `employee` INNER JOIN `access` ON employee.accessID=access.accessID INNER JOIN `accesspermission` ON access.accessID=accesspermission.accessId INNER JOIN `permissions` ON accesspermission.permId=permissions.permID WHERE accesspermission.val=1 && permissions.category=4 GROUP BY employee.empID HAVING COUNT(permissions.permId)=(SELECT COUNT(*) FROM `permissionscategory` A INNER JOIN `permissions` B ON A.categoryID=B.category WHERE A.categoryID=4);';
+/*
+$sql = 'SELECT employee.empID, employee.accessID,COUNT(permissions.permId) FROM `employee` 
+INNER JOIN `access` ON employee.accessID=access.accessID 
+INNER JOIN `accesspermission` ON access.accessID=accesspermission.accessId 
+INNER JOIN `permissions` ON accesspermission.permId=permissions.permID 
+WHERE accesspermission.val=1 && permissions.category=4 
+GROUP BY employee.empID 
+HAVING COUNT(permissions.permId) = (
+    SELECT COUNT(*) FROM `permissionscategory` A 
+    INNER JOIN `permissions` B ON A.categoryID=B.category 
+    WHERE A.categoryID=4
+);';
+*/
+
+if(getFutureFullAccountAccessCount($accessID, $_POST['perms'])==0) {
+    echo $output->setFailed("Atleast 1 role must have full account management access");
+    die();
+}
+
+if(getFutureFullAccountUserCount($accessID, $_POST['perms'])==0) {
+    echo $output->setFailed("Atleast 1 account must have full account management access");
+    die();
+}
+
 #print_r($formatedPerms);
 
+#die();
 
 foreach($formatedPerms as $permID => &$val) {
     prepareForSQL($conn, $val, 0);
     $sql .= "UPDATE accesspermission SET val=$val WHERE accessId=".$accessID." && permId=$permID;\n";
-}
+}   
 
 if(mysqli_multi_query($conn, $sql)) {
     #echo "AF rows: ".mysqli_affected_rows($conn);
