adb957e65f553b1d384f79b013f35d4673b95624
joshuamilante <milante.joshuajazon@ue.edu.ph>
2021-11-11 01:06:35 +0800

    Revert "try a href"
    This reverts commit 95d34fd73100e2909b624585d808973f93055853.

--- customer/Customer-Booking_Form.php
+++ customer/Customer-Booking_Form.php
@@ -489,6 +489,7 @@ $followingdata = $result->fetch_array(MYSQLI_ASSOC);
 								<td><br></td>
 							</tr>
 							<tr align="right">
+
 								<td colspan="2"><button type="submit" name="submit" id="submit" class="btn btn-success">Proceed to Payment</button></td>
 							</tr>
 					</form>
@@ -512,7 +513,7 @@ $followingdata = $result->fetch_array(MYSQLI_ASSOC);
 			</div>
 			<div class="col-lg-4 mx-auto">
 				<p>Connect with us at</p>
-				<a href="<?php echo $followingdata["socialFB"]; ?>"><button type="button" class="btn btn-social-icon btn-facebook btn-rounded" ><i class="fa fa-facebook"></i></button></a>
+				<button type="button" class="btn btn-social-icon btn-facebook btn-rounded" href="<?php echo $followingdata["socialFB"]; ?>"><i class="fa fa-facebook"></i></button>
 				<button type="button" class="btn btn-social-icon btn-instagram btn-rounded" href="<?php echo $followingdata["socialInstagram"]; ?>"><i class="fa fa-instagram"></i></button>
 				<button type="button" class="btn btn-social-icon btn-twitter btn-rounded" href="<?php echo $followingdata["socialTwitter"]; ?>"><i class="fa fa-twitter"></i></button>
 			</div>
