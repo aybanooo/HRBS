5c6d73a99b2ceecb36a877b4167cc155de9cf63b
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-10 09:27:23 +0800

    dev: update permissions table and add new permission constant

--- admin/customFiles/php/database/permissionControls/permissionHandler.php
+++ admin/customFiles/php/database/permissionControls/permissionHandler.php
@@ -9,31 +9,32 @@ require_once __F_FORMAT__;
 //--------------- PERMISSION LIST ---------------
 
 /*
-SELECT A.*, B.name FROM `permissions` A INNER JOIN `permissionscategory` B ON A.category=B.categoryID;
+SELECT A.*, B.name FROM `permissions` A LEFT JOIN `permissionscategory` B ON A.category=B.categoryID ORDER BY `category`;
 +--------+----------------------+----------+-----------+
 | permID | name                 | category | name      |
 +--------+----------------------+----------+-----------+
+|     20 | General Testing      |        0 | NULL      |
+|     21 | Manage Reservations  |        0 | NULL      |
 |      1 | Add/Delete Rooms     |        1 | Rooms     |
-|      2 | Manage room thumbnai |        1 | Rooms     |
-|      3 | Manage room descript |        1 | Rooms     |
-|      4 | Manage room sections |        1 | Rooms     |
-|      5 | Manage room general  |        1 | Rooms     |
 |      6 | Manage room rates    |        1 | Rooms     |
+|      5 | Manage room general  |        1 | Rooms     |
+|      4 | Manage room sections |        1 | Rooms     |
+|      3 | Manage room descript |        1 | Rooms     |
+|      2 | Manage room thumbnai |        1 | Rooms     |
+|     11 | Manage amenities     |        2 | Amenities |
+|     19 | Manage Vouchers      |        3 | Vouchers  |
+|     22 | Change Account Role  |        4 | Accounts  |
+|     10 | Manage roles         |        4 | Accounts  |
 |      7 | Add accounts         |        4 | Accounts  |
 |      8 | Delete accounts      |        4 | Accounts  |
 |      9 | Reset password       |        4 | Accounts  |
-|     10 | Manage roles         |        4 | Accounts  |
-|     11 | Manage amenities     |        2 | Amenities |
-|     12 | Modify company name  |        5 | Webpage   |
-|     13 | Modify Page Cover    |        5 | Webpage   |
-|     14 | Modify Company Logo  |        5 | Webpage   |
-|     15 | Modify Contact Info  |        5 | Webpage   |
-|     16 | Modify Social Media  |        5 | Webpage   |
-|     17 | Modify Display       |        5 | Webpage   |
 |     18 | Modify Location      |        5 | Webpage   |
-|     19 | Manage Vouchers      |        3 | Vouchers  |
-|     20 | General Testing      |        0 | NULL      |
-|     21 | Manage Reservations  |        0 | NULL      |
+|     17 | Modify Display       |        5 | Webpage   |
+|     16 | Modify Social Media  |        5 | Webpage   |
+|     15 | Modify Contact Info  |        5 | Webpage   |
+|     14 | Modify Company Logo  |        5 | Webpage   |
+|     13 | Modify Page Cover    |        5 | Webpage   |
+|     12 | Modify company name  |        5 | Webpage   |
 +--------+----------------------+----------+-----------+
 */
 
@@ -54,6 +55,8 @@ define('__V_P_ACCOUNT_DELETE__', explode(',',
 '8' ));
 define('__V_P_ACCOUNT_RESET_PASS__', explode(',',
 '9' ));
+define('__V_P_ACCOUNT_CHANGE_ROLE__', explode(',',
+'22' ));
 
 //--------------- PERMISSION LIST END ---------------
 
