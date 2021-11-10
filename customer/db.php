f347aff5f0299d67412ebad13d9962ec54dbd774
joshuamilante <milante.joshuajazon@ue.edu.ph>
2021-11-11 00:25:25 +0800

    conn change name from con

--- customer/db.php
+++ customer/db.php
@@ -5,9 +5,9 @@ $username = "u362912910_thanos2";
 $password = "L]5py5jrlHY";
 $dbname = "u362912910_hrbs2";
 
-$con = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($con->connect_error) {
+if ($conn->connect_error) {
     $output->setFailed("Cannot connect to database." . $conn->connect_error);
     echo $output->getOutput(true);
     die();
