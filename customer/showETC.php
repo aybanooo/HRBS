e080b30a4802d0b40f191159ee3b8bff020de71b
joshuamilante <milante.joshuajazon@ue.edu.ph>
2021-11-13 11:31:46 +0800

    rate update

--- customer/showETC.php
+++ customer/showETC.php
@@ -3,11 +3,11 @@ include('db.php');
 
 $valueFromBooking = $_POST['id'];
 
-$query="SELECT rate FROM roomType WHERE `name`='{$valueFromBooking}'";
+$query="SELECT rate FROM roomtype WHERE `name`='{$valueFromBooking}'";
 $result=mysqli_query($conn, $query) or die(mysqli_error($conn));
 while($row=mysqli_fetch_array($result)){
 ?>
-        <td value="<?php echo $row['rate']?>" name="roomRate"><?php echo $row["rate"]; ?></td>
+        <td value="<?php echo $row["rate"]?>" name="roomRate"><?php echo $row["rate"]; ?></td>
 <?php
   }  
 ?>
