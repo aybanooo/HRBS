cfd66987af042d0bb29cf4dc0846da0702349d9e
joshuamilante <milante.joshuajazon@ue.edu.ph>
2021-11-13 11:10:53 +0800

    rate fix

--- customer/showETC.php
+++ customer/showETC.php
@@ -7,7 +7,7 @@ $query="SELECT rate FROM roomtype WHERE `name`='{$valueFromBooking}'";
 $result=mysqli_query($conn, $query) or die(mysqli_error($conn));
 while($row=mysqli_fetch_array($result)){
 ?>
-        <td value="<?php echo $row['rate']?>" name="roomRate"><?php echo $row["rate"]; ?></td>
+        <td value="<?php echo $row["rate"]?>" name="roomRate"><?php echo $row["rate"]; ?></td>
 <?php
   }  
 ?>
