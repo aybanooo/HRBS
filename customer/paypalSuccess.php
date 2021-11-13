d0b9b76f325303ada1cfd399e83ffe34fbe248a0
honoist <90629587+honoist@users.noreply.github.com>
2021-11-13 13:40:44 +0800

    Sa email

--- customer/paypalSuccess.php
+++ customer/paypalSuccess.php
@@ -1,51 +1,20 @@
 <?php
 include('db.php');
 
- //Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-//require_once 'email-config.php';
-require 'vendor/autoload.php';
-
-//Create an instance; passing true enables exceptions
-$mail = new PHPMailer(true);
-$apiKeyPass = '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';
-
-
-try {
-    //Server settings
-    $mail->SMTPDebug = $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.sendgrid.net';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'apikey';                     //SMTP username
-    $mail->Password   = $apiKeyPass;                               //SMTP password
-    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS
-
-    //Recipients
-    $mail->setFrom('Thanosemailthesis@gmail.com', 'Thanos');
-    $mail->addAddress('benjbenito10@gmail.com');  //Add a recipient
-
-    $body = '<p><strong>Hello<strong/>This is a test email<p/>';
-
-
-    //Content
-
-    $mail->Subject = 'Test email';
-    $mail->Body    = $body;
-    $mail->AltBody = strip_tags($body);
-
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+$from = "thanoshotelreservation@ghrbs.site";
+$to = "$email";
+$subject = "Checking PHP mail";
+$message = "PHP mail works just fine";
+$headers = "From:" . $from;
+if(mail($to,$subject,$message, $headers)) {
+echo "The email message was sent.";
+} else {
+echo "The email message was not sent.";
 }
+
 
 $query = "SELECT companyName FROM companyinfo";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
