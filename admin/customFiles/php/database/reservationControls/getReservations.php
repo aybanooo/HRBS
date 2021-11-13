7534c9106b67f2b4aa2dbaf2c925e20bce825aa5
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-13 08:17:34 +0800

    dev: create php script for fetching reservations data

---
+++ admin/customFiles/php/database/reservationControls/getReservations.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once(dirname(__FILE__, 3)."/directories/directories.php");
+require_once __F_DB_HANDLER__;
+
+
+$sql = 'SELECT A.*, CONCAT(B.`fname`, " ", B.`lname`) AS Name, B.`contact`, B.`email`, B.`verified`, B.`verification` FROM `reservation` A LEFT JOIN `customer` B ON A.`customerID`=B.`customerID`;';
+
+$tempConn = createTempDBConnection();
+
+$result = mysqli_fetch_all(mysqli_query($tempConn, $sql), MYSQLI_ASSOC);
+
+echo json_encode(["data"=>$result]);
+return;
+?>
+
+<pre>
+    <?php
+        echo json_encode($result);
+    ?>
+</pre>
