fd3d80010f6f5ff93a2bd1077ceb2923f9132b54
joshuamilante <milante.joshuajazon@ue.edu.ph>
2021-11-14 09:40:56 +0800

    Update Customer-Rooms.php

--- customer/Customer-Rooms.php
+++ customer/Customer-Rooms.php
@@ -174,7 +174,7 @@ $followingdata = $result->fetch_array(MYSQLI_ASSOC);
 									<h1><b><?php echo $row["name"]; ?></b></h1>
 								</div>
 								<div class="col-sm-4">
-									<a href="<?php $row["url"]; ?>"><button type="button" class="btn btn-primary" >Book a Room</button></a>
+									<a href="<?php echo $row["url"]; ?>"><button type="button" class="btn btn-primary" >Book a Room</button></a>
 								</div>
 							</div>	
 								<p><?php echo $row["desc"]; ?></p>
