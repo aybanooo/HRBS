3945ee4288f5044e7a177fa969805f25b28e1b6e
Xylo10 <benjbenito10@gmail.com>
2021-11-16 17:20:05 +0800

    email template
    test email template

---
+++ customer/email-test-test-test.php
@@ -0,0 +1,56 @@
+<?php
+
+include('db.php');
+$maxIDQ = "SELECT MAX(email) AS 'maxID',  FROM customer";
+$maxIDRes = mysqli_query($conn, $maxIDQ);
+$maxIDRow = mysqli_fetch_assoc($maxIDRes);
+$customerEmail = $maxIDRow['maxID'];
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php';
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->Port = 465;
+    $mail->SMTPAuth = true;
+    $mail->Username = 'thanoshotelreservation@ghrbs.site';
+    $mail->Password = 'Thanos123';
+    $mail->setFrom('test@hostinger-tutorials.com', 'Mr. Drago');
+    $mail->addAddress('benjbenito10@gmail.com', 'Valued Guest');
+    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+        $mail->Subject = 'PHPMailer contact form';
+        $mail->isHTML(false);
+        $mail->Body = <<<EOT
+Email: {$_POST['email']}
+Name: {$_POST['name']}
+Message: {$_POST['message']}
+EOT;
+        if (!$mail->send()) {
+            $msg = 'Sorry, something went wrong. Please try again later.';
+        } else {
+            $msg = 'Message sent! Thanks for contacting us.';
+        }
+    } else {
+        $msg = 'Share it with us!';
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact form</title>
+</head>
+<body>
+<h1>Do You Have Anything in Mind?</h1>
+<?php if (!empty($msg)) {
+    echo "<h2>$msg</h2>";
+} ?>
+<form method="POST">
+    <label for="name">Name: <input type="text" name="name" id="name"></label><br><br>
+    <label for="email">Email: <input type="email" name="email" id="email"></label><br><br>   
+    <label for="message">Message: <textarea name="message" id="message" rows="8" cols="20"></textarea></label><br><br>
+    <input type="submit" value="Send">
+</form>
+</body>
+</html>
