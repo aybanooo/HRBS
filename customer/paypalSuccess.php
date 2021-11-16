ce2d1024c280cb012ab52c29a095f5f531650dde
Xylo10 <benjbenito10@gmail.com>
2021-11-16 18:49:42 +0800

    Update paypalSuccess.php
    revert changes

--- customer/paypalSuccess.php
+++ customer/paypalSuccess.php
@@ -1,35 +1,21 @@
 <?php
 include('db.php');
-#$maxIDQ = "SELECT MAX(email) AS 'maxID' FROM customer";
-#$maxIDRes = mysqli_query($conn, $maxIDQ);
-#$maxIDRow = mysqli_fetch_assoc($maxIDRes);
-#$customerEmail = $maxIDRow['maxID'];
-#ini_set( 'display_errors', 1 );
-#error_reporting( E_ALL );
-$apiKey = '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';
-    use PHPMailer\PHPMailer\PHPMailer;
-    require 'vendor/autoload.php';
-    $mail = new PHPMailer;
-    $mail->isSMTP();
-    $mail->SMTPDebug = 2;
-    $mail->Host = 'smtp.sendgrid.net';
-    $mail->Port = 587;
-    $mail->SMTPAuth = true;
-    $mail->Username = 'apikey';
-    $mail->Password = $apiKey;
-    $mail->setFrom('thanoshotelreservation@ghrbs.site', 'Thanos');
-    $mail->addReplyTo('thanoshotelreservation@ghrbs.site', 'Thanos');
-    $mail->addAddress('benjbenito10@gmail.com', 'Valued Guest');
-    $mail->Subject = 'Testing PHPMailer';
-    $mail->msgHTML(file_get_contents('message.html'), __DIR__);
-    $mail->Body = 'This is a plain text message body';
-    //$mail->addAttachment('test.txt');
-    if (!$mail->send()) {
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
-    } else {
-        echo 'The email message was sent.';
-    }
-
+$maxIDQ = "SELECT MAX(email) AS 'maxID' FROM customer";
+$maxIDRes = mysqli_query($conn, $maxIDQ);
+$maxIDRow = mysqli_fetch_assoc($maxIDRes);
+$customerEmail = $maxIDRow['maxID'];
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+$from = "thanoshotelreservation@ghrbs.site";
+$to = $customerEmail;
+$subject = "Booking Details";
+$message = "PHP mail works just fine";
+$headers = "From:" . $from;
+if(mail($to,$subject,$message, $headers)) {
+echo "The email message was sent.";
+} else {
+echo "The email message was not sent.";
+}
 
 $query = "SELECT companyName FROM companyinfo";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
