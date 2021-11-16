9611359dbcdef2506c2efaa129af4d5fcd82ab20
Xylo10 <benjbenito10@gmail.com>
2021-11-16 21:08:17 +0800

    Update email-test-test-test.php

--- customer/email-test-test-test.php
+++ customer/email-test-test-test.php
@@ -16,8 +16,8 @@ $apiKey = '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
     $mail->Host = 'smtp.hostinger.com';
     $mail->Port = 465;
     $mail->SMTPAuth = true;
-    $mail->Username = 'apikey';
-    $mail->Password = $apiKey;
+    $mail->Username = 'thanoshotelreservation@ghrbs.site';
+    $mail->Password = 'Thanos123';
     $mail->setFrom('thanoshotelreservation@ghrbs.site', 'Thanos');
     $mail->addReplyTo('thanoshotelreservation@ghrbs.site', 'Thanos');
     $mail->addAddress('benjbenito10@gmail.com', 'Benj');
