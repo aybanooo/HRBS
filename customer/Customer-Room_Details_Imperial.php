10e90f39a171ca53aef42d4089ae17499acbbf7e
joshuamilante <milante.joshuajazon@ue.edu.ph>
2021-11-17 09:32:11 +0800

    rate children adults div

--- customer/Customer-Room_Details_Imperial.php
+++ customer/Customer-Room_Details_Imperial.php
@@ -350,13 +350,13 @@ $followingdata = $result->fetch_array(MYSQLI_ASSOC);
                                 <div class="card">
                                     <div class="card-body">
                                         <h4 class="card-title">Adult</h4>
-                                        <p class="card-text"><?php echo $followingdataGuest["maxAdult"]; ?> Adult</p>
+                                        <p class="card-text"><?php echo $followingdataGuest["maxAdult"]; ?></p>
                                     </div>
                                 </div>
                                 <div class="card">
                                     <div class="card-body">
                                         <h4 class="card-title">Children</h4>
-                                        <p class="card-text"><?php echo $followingdataGuest["maxAdult"]; ?> Children</p>
+                                        <p class="card-text"><?php echo $followingdataGuest["maxChildren"]; ?></p>
                                     </div>
                                 </div>
                             </div>
