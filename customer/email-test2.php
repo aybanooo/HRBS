84c45f572559a11da972ae7c8bbbe01fc130c884
Xylo10 <benjbenito10@gmail.com>
2021-11-17 21:31:23 +0800

    Update email-test2.php

--- customer/email-test2.php
+++ customer/email-test2.php
@@ -8,30 +8,29 @@ include('db.php');
 #ini_set( 'display_errors', 1 );
 #error_reporting( E_ALL );
 $apiKey = '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';
-#$mail->Username = 'apikey';
-    #$mail->Password = $apiKey;
-    #$mail->setFrom('thanoshotelreservation@ghrbs.site', 'Thanos');
-    ##$mail->addReplyTo('thanoshotelreservation@ghrbs.site', 'Thanos');
-    #$mail->addAddress('benjbenito10@gmail.com', 'Benj');
-    #$mail->Host = 'smtp.sendgrid.net';
-    #$mail->Port = 587;
-$subject = "testing 123";
-use PHPMailer\PHPMailer\PHPMailer;
-require 'vendor/autoload.php';
-$mail = new PHPMailer;
-$mail->SMTPDebug = 2;
-$mail->Host = 'smtp.sendgrid.net';
-$mail->Port = 587;
-$mail->Username = 'apikey';
-$mail->Password = $apiKey;
-$mail->IsSMTP(true);
-$mail->CharSet = "utf-8";
-$mail->From = "thanoshotelreservation@ghrbs.site";
-$mail->FromName = "GHRBS";
-$mail->addReplyTo("thanoshotelreservation@ghrbs.site", "Reply Address");
-$mail->addAddress("benjbenito10@gmail.com"); 
-$mail->Subject  = "Booking Details";
-$mail->isHTML(true);
+    use PHPMailer\PHPMailer\PHPMailer;
+    require 'vendor/autoload.php';
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->SMTPDebug = 2;
+    $mail->Host = 'smtp.sendgrid.net';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->Username = 'apikey';
+    $mail->Password = $apiKey;
+    $mail->setFrom('thanoshotelreservation@ghrbs.site', 'Thanos');
+    $mail->addReplyTo('thanoshotelreservation@ghrbs.site', 'Thanos');
+    #send email to
+    $mail->addAddress($result, 'Valued Guest');
+    $mail->Subject = 'GHRBS booking details';
+    
+    $mail->Body = 'This is a plain text message body';
+    //$mail->addAttachment('test.txt');
+    if (!$mail->send()) {
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'The email message was sent.';
+    }
 $mail->Body = "<html><body><p><b>Booking ID: bookingID</b></p><pWe look forward to welcoming you to our resort on checkinDate.</p><br><p>Our professional and friendly staff are committed to ensuring your stay is both enjoyable and comfortable.</p><br><p>Should you have any requests prior to your stay, please do not hesitate to contact us at thanoshotelreservation@ghrbs.site  and we will endeavor to assist you whenever possible.</p><br><br><p>Thanks & Best Regards,</p><br><p>GHRBS team</p></body></html>";
 $mail->AltBody = "Booking ID: bookingID\n\nWe look forward to welcoming you to our resort on checkinDate.\n\nOur professional and friendly staff are committed to ensuring your stay is both enjoyable and comfortable.\n\nShould you have any requests prior to your stay, please do not hesitate to contact us at thanoshotelreservation@ghrbs.site  and we will endeavor to assist you whenever possible.\n\nThanks & Best Regards,\n\n
 GHRBS team";
