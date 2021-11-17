fcd7fbead663bd204231c6b7b9dbf20ef712baf9
joshuamilante <milante.joshuajazon@ue.edu.ph>
2021-11-17 20:24:10 +0800

    review arigathanks

--- customer/reviewThanks.php
+++ customer/reviewThanks.php
@@ -1,30 +1,22 @@
 <?php
 include('db.php');
 
-require_once(dirname(__FILE__,2)."/public_assets/modules/php/directories/directories.php");
-
 $query = "SELECT companyName FROM companyinfo";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 $followingdata = $result->fetch_array(MYSQLI_ASSOC);
-
-    if(isset($_POST['submit'])){ 
-        $comments = $_POST['commentsSuggest']; 
-        $reservationID = $_POST['reservationID'];
-
-        $queryReviews = "INSERT INTO reviews (reviewID, reservationID, review) VALUES (DEFAULT ,$reservationID, '$comments');";
-        mysqli_query($conn, $queryReviews) or die(mysqli_error($conn));
-    }
 ?>
+
+
 
 <!DOCTYPE HTML>
 <html lang="en">
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title><?php echo $followingdata['companyName']; ?> | Customer Review</title>
+    <title>Thank you for booking with us!</title>
     <link rel="icon" type="image/x-icon" href="" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -35,89 +27,53 @@ $followingdata = $result->fetch_array(MYSQLI_ASSOC);
             padding-top: 65px;
             background-color: #F2F2F3;
         }
+
         #mainNav .navbar-brand {
             color: rgba(255, 255, 255, 1);
         }
+
         #mainNav .nav-link {
             color: rgba(255, 255, 255, 1);
         }
+
         nav#mainNav {
             background-color: black;
             position: fixed;
         }
-        .loginForm h1 {
+
+        h1 {
             text-align: center;
             padding-top: 2%;
             padding-bottom: 2%;
-            font-size: 2em;
         }
-        .loginForm {
+
+        .boxinfo {
             background-color: white;
             color: black;
             padding: 2%;
             text-align: center;
-            margin: 5% auto;
-            width: 50%;
-            box-shadow: 0 0 5px 0 rgb(0 0 0 / 40%);
+            margin: 8% auto;
+            width: 40%;
+            box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.4);
             border-radius: 5px;
         }
 
-        input[type=text],
-        input[type=password] {
-
-            border: 1px solid #c1c1c1;
-            background-color: #eee;
-            color: #000;
-            width: 35%;
-            margin: auto;
-            display: block;
-            margin-bottom: 4%;
-            padding: 10px;
-            border-radius: 25px;
-            outline: 0;
-            padding-left: 15px;
-            font-size: 1em;
+        .boxinfo h1 {
             text-align: center;
         }
-        textarea#exampleFormControlTextarea1{
-            width: 75%;
-            margin: auto;
-        }
-        input[type=submit] {
-            border: none;
-            outline: 0;
-            width: 35%;
-            padding: 10px;
-            font-size: .8em;
-            border-radius: 25px;
-            background-color: #2980B9;
-            color: #fff;
-            display: block;
-            margin: auto;
-            margin-bottom: 5%;
-            margin-top: 5%;
-            font-size: 1.2em;
-        }
 
-        input[type=submit]:hover {
-            box-shadow: 0 0 5px 1px rgb(0, 0, 0, 0.2), 0 0 2px 0 rgb(0, 0, 0, 0.5);
-            background-color: #2471A3;
-            transition: 0.3s;
-            transform: scale(1.005);
+        .boxinfo p {
+            text-align: center;
         }
 
         .footer {
             background-color: black;
             color: rgba(255, 255, 255, .8);
             padding: 1%;
-            width: 100%;
-            text-align: center;
             position: absolute;
             bottom: 0;
-        }
-
-        textarea {
-            resize: none;
+            width: 100%;
+            text-align: center;
         }
 
         .template-demo>.btn {
@@ -150,7 +106,7 @@ $followingdata = $result->fetch_array(MYSQLI_ASSOC);
 
         .btn-instagram {
             background: #dc4a38;
-            color: #ffffff;
+            color: #ffffff
         }
 
         .btn-facebook:hover,
@@ -170,17 +126,12 @@ $followingdata = $result->fetch_array(MYSQLI_ASSOC);
             background: #bf3322;
             color: #ffffff
         }
-
-        .new1 {
-            border-top: 1px solid #999;
-            width: 80%;
-            margin-bottom: 25px;
-        }
     </style>
-    <title>Login</title>
+    <title>Sign Up</title>
 </head>
 
 <body>
+
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
         <div class="container">
             <a class="navbar-brand js-scroll-trigger" href="Customer-Home.php"><?php echo $followingdata['companyName']; ?></a>
@@ -190,36 +141,15 @@ $followingdata = $result->fetch_array(MYSQLI_ASSOC);
             </button>
         </div>
     </nav>
-    <form action="reviewThanks.php" method="POST">
-    <section id="login">
-        <div class="loginForm">
-            <div class="row">
-                <div class="col-lg-12 mx-auto">
-                    <h1><b>Comments and Suggestions</b></h1>
-                    <hr class="new1">
-                    <form method="POST" action="">
-                        <tr align="left">
-                            <td><input type="text" name="reservationID" required placeholder="Reservation ID"></td>
-                        </tr>
-                        <tr>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" name="commentsSuggest" rows="3" placeholder="Enter Comments/Suggestion and Rating"></textarea>
-                        </tr>
-                        <tr>
-                            <td class="sign" align="center" align="right"><input type="submit" value="Send Review" name="submit"></td>
-                        </tr>
-                    </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    </form>
     <?php
     $query = "SELECT socialFB, socialTwitter, socialInstagram, contact, email, footerRight
         FROM socialmedias, companyinfo";
     $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
     $followingdata = $result->fetch_array(MYSQLI_ASSOC);
     ?>
+    <div class="boxinfo">
+        <h1>Thank you for your comment and suggestion</h1>
+    </div>
     <div class="footer">
         <div class="row">
             <div class="col-lg-4 mx-auto">
@@ -238,5 +168,7 @@ $followingdata = $result->fetch_array(MYSQLI_ASSOC);
             </div>
         </div>
     </div>
+
 </body>
+
 </html>
