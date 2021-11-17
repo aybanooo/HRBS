b6f69be6700051a03be989a144ac24739c0d5b20
Xylo10 <benjbenito10@gmail.com>
2021-11-17 21:28:16 +0800

    Update email-test2.php

--- customer/email-test2.php
+++ customer/email-test2.php
@@ -19,6 +19,7 @@ $subject = "testing 123";
 use PHPMailer\PHPMailer\PHPMailer;
 require 'vendor/autoload.php';
 $mail = new PHPMailer;
+$mail->SMTPDebug = 2;
 $mail->Host = 'smtp.sendgrid.net';
 $mail->Port = 587;
 $mail->Username = 'apikey';
@@ -31,9 +32,14 @@ $mail->addReplyTo("thanoshotelreservation@ghrbs.site", "Reply Address");
 $mail->addAddress("benjbenito10@gmail.com"); 
 $mail->Subject  = "Booking Details";
 $mail->isHTML(true);
-$mail->Body = "<html><body><p><b>Booking ID: bookingID</b></p><pWe look forward to welcoming you to our resort on $checkinDate.</p><br><p>Our professional and friendly staff are committed to ensuring your stay is both enjoyable and comfortable.</p><br><p>Should you have any requests prior to your stay, please do not hesitate to contact us at thanoshotelreservation@ghrbs.site  and we will endeavor to assist you whenever possible.</p><br><br><p>Thanks & Best Regards,</p><br><p>GHRBS team</p></body></html>";
+$mail->Body = "<html><body><p><b>Booking ID: bookingID</b></p><pWe look forward to welcoming you to our resort on checkinDate.</p><br><p>Our professional and friendly staff are committed to ensuring your stay is both enjoyable and comfortable.</p><br><p>Should you have any requests prior to your stay, please do not hesitate to contact us at thanoshotelreservation@ghrbs.site  and we will endeavor to assist you whenever possible.</p><br><br><p>Thanks & Best Regards,</p><br><p>GHRBS team</p></body></html>";
 $mail->AltBody = "Booking ID: bookingID\n\nWe look forward to welcoming you to our resort on checkinDate.\n\nOur professional and friendly staff are committed to ensuring your stay is both enjoyable and comfortable.\n\nShould you have any requests prior to your stay, please do not hesitate to contact us at thanoshotelreservation@ghrbs.site  and we will endeavor to assist you whenever possible.\n\nThanks & Best Regards,\n\n
 GHRBS team";
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'The email message was sent.';
+}
 ?>
  
      
