e33f86323ed2f56951b0c0d307b86bca0a938ac7
Xylo10 <benjbenito10@gmail.com>
2021-11-17 21:45:26 +0800

    Update paypalSuccess.php

--- customer/paypalSuccess.php
+++ customer/paypalSuccess.php
@@ -23,16 +23,23 @@ $apiKey = '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
     $mail->setFrom('thanoshotelreservation@ghrbs.site', 'Thanos');
     $mail->addReplyTo('thanoshotelreservation@ghrbs.site', 'Thanos');
     #send email to
-    $mail->addAddress($result, 'Valued Guest');
+    $mail->addAddress('benjbenito10@gmail.com', 'Valued Guest');
     $mail->Subject = 'GHRBS booking details';
-    
-    $mail->Body = 'This is a plain text message body';
     //$mail->addAttachment('test.txt');
     if (!$mail->send()) {
         echo 'Mailer Error: ' . $mail->ErrorInfo;
     } else {
         echo 'The email message was sent.';
     }
+$mail->Body = "<html><body><p><b>Booking ID: bookingID</b></p><p>We look forward to welcoming you to our resort on checkinDate.</p><br>Our professional and friendly staff are committed to ensuring your stay is both enjoyable and comfortable.</p><br><p>Should you have any requests prior to your stay, please do not hesitate to contact us at thanoshotelreservation@ghrbs.site and we will endeavor to assist you whenever possible.<br><p>Thanks & Best Regards,</p><<p>GHRBS team</p></body></html>";
+$mail->AltBody = "Booking ID: bookingID\n\nWe look forward to welcoming you to our resort on checkinDate.\n\nOur professional and friendly staff are committed to ensuring your stay is both enjoyable and comfortable.\n\nShould you have any requests prior to your stay, please do not hesitate to contact us at thanoshotelreservation@ghrbs.site  and we will endeavor to assist you whenever possible.\n\nThanks & Best Regards,\n\n
+GHRBS team";
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'The email message was sent.';
+}
+?>
 
 $query = "SELECT companyName FROM companyinfo";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
