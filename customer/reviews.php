fddb25907b06d78cb2fa77b4bb87868b34b5e2f7
joshuamilante <milante.joshuajazon@ue.edu.ph>
2021-11-17 09:15:40 +0800

    reviews page footer

--- customer/reviews.php
+++ customer/reviews.php
@@ -109,6 +109,7 @@ $followingdata = $result->fetch_array(MYSQLI_ASSOC);
             width: 100%;
             text-align: center;
             position: absolute;
+            bottom: 0;
         }
 
         textarea {
