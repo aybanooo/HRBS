14f4a8a7a827be8f7d334bd5143bb765703bd213
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-20 18:38:17 +0800

    dev: implement including footer php script

--- admin/admin-billing.php
+++ admin/admin-billing.php
@@ -171,14 +171,7 @@ unset($result, $conn_displayable_data);
   <!-- /.content-wrapper -->
 
   <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2020-2021 <a href="#">Link Here</a>.</strong> All rights reserved.
-  </footer>
+  <?php include(__F_MAIN_FOOTER__); ?>
 </div>
 <!-- ./wrapper -->
 
--- admin/admin-webAnalytics.php
+++ admin/admin-webAnalytics.php
@@ -369,14 +369,7 @@ setupUserSession();
   <!-- /.content-wrapper -->
 
   <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2020-2021 <a href="#">Link Here</a>.</strong> All rights reserved.
-  </footer>
+  <?php include(__F_MAIN_FOOTER__); ?>
 </div>
 <!-- ./wrapper -->
 
