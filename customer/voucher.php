468a01149388e81c7ac99add4214dcb93b501be0
joshuamilante <milante.joshuajazon@ue.edu.ph>
2021-11-20 21:45:05 +0800

    Update voucher.php

--- customer/voucher.php
+++ customer/voucher.php
@@ -2,18 +2,20 @@
 include('db.php');
 	$coupon_code = $_POST['coupon'];
 	$price = $_POST['price'];
-	$room = $_POST['roomName'];
  
-	$query = mysqli_query($conn, "SELECT * FROM `promotion` WHERE `promoCode` = '$coupon_code' &&  `minSpend` <= '$price' && `maxSpend` >= '$price' && `expiry` != 'CURDATE()';" ) or die(mysqli_error($conn));
+	$query = mysqli_query($conn, "SELECT * FROM `promotion` WHERE `promoCode` = '$coupon_code' && `value` = '1' && `minSpend` <= '$price' && `maxSpend` >= '$price'") or die(mysqli_error($conn));
 	$count = mysqli_num_rows($query);
 	$fetch = mysqli_fetch_array($query);
 	$array = array();
 
 
 	 	if($count > 0){
-			$total = $price - $fetch['value'];
+			$discount = $fetch['discount'] / 100;
+			$total = $discount * $price;
+			$array['discount'] = $fetch['discount'];
+			$array["price"] = $price-$total;
 			
-			echo json_encode($total);
+			echo json_encode($array);
 			
 		}else{
 			echo "error";
