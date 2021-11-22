ff4f0d41bed368c94e7bb95e79f2ec6616547ed4
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-22 17:36:07 +0800

    dev: add php script to get select node base on checkin/out date

---
+++ public_assets/modules/php/database/reservationControls/getAvailableRoomsAsSelect.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once(dirname(__FILE__, 3)."/directories/directories.php");
+require_once __F_DB_HANDLER__;
+require_once __F_OUTPUT_HANDLER__;
+require_once __F_RSV_HANDLER__;
+
+(!isset($_GET['d'])) && die();
+
+$date = decodeCheckinoutDate($_GET['d']);
+
+if(!$date) die;
+
+$roomIDs = getBookableRoomsID($date[0], $date[1]);
+
+$rooms = getRoomDetails($roomIDs);
+?>
+
+<select id="nameRoom" name="roomName" name="pickRoom" onchange="selectRate()">
+<?php foreach($rooms as $room) { ?>
+    <option value="<?php print $room['roomTypeID'];?>"><?php print $room['name'];?></option>
+<?php } ?>
+</select>
