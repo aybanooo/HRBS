2abf3f9c6d8bf28d9ba08807bebdb666f6127944
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-22 20:24:14 +0800

    fix showETC: change roomname condition to roomtypeid

--- customer/showETC.php
+++ customer/showETC.php
@@ -3,9 +3,12 @@ include('db.php');
 
 // Iescape natin para iwas SQL injection. Ito din dahilan ng error kapag may quote yung query. 
 // Bakit nga pala name ng room yung value? hindi ba dapat ID ng room?
-$valueFromBooking = mysqli_real_escape_string($conn, $_POST['id']); 
+if(isset($_POST['id']) && $_POST['id']!="")
+  $valueFromBooking = mysqli_real_escape_string($conn, $_POST['id']);
+else
+  die();
 
-$query="SELECT rate FROM roomtype WHERE `name`='{$valueFromBooking}'";
+$query="SELECT rate FROM roomtype WHERE `roomTypeID`={$valueFromBooking} LIMIT 1;";
 $result=mysqli_query($conn, $query) or die(mysqli_error($conn));
 while($row=mysqli_fetch_array($result)){
 ?>
