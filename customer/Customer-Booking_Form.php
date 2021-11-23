64848c3c901c525f8e06d047c3f3a4e75427b8db
Xylo10 <benjbenito10@gmail.com>
2021-11-23 17:11:39 +0800

    Update Customer-Booking_Form.php

--- customer/Customer-Booking_Form.php
+++ customer/Customer-Booking_Form.php
@@ -466,9 +466,9 @@ if(isset($_GET['d']) && $_GET['d'] != "") {
 								<?php
 								if (isset($_POST['adults'])) {
 									$adults = $_POST['adults'];
-									echo '<td><input id="noGuest" type="number" name="adults" placeholder="No. of Adults" value="' . $adults . '" min="0" required></td>';
+									echo '<td><input id="noGuest" type="number" name="adults" placeholder="No. of Adults" value="' . $adults . '" min="0" value="0"></td>';
 								} else {
-									echo '<td><input id="noGuest" type="number" name="adults" placeholder="No. of Adults" required min="0" value="1"></td>';
+									echo '<td><input id="noGuest" type="number" name="adults" placeholder="No. of Adults" required min="0" required></td>';
 								}
 								?>
 							</tr>
@@ -477,9 +477,9 @@ if(isset($_GET['d']) && $_GET['d'] != "") {
 								<?php
 								if (isset($_POST['children'])) {
 									$children = $_POST['children'];
-									echo '<td><input id="noGuest" type="number" name="children" placeholder="No. of Childrens" value="' . $children . '"  min="0" required></td>';
+									echo '<td><input id="noGuest" type="number" name="children" placeholder="No. of Childrens" value="' . $children . '"  min="0" value="0"></td>';
 								} else {
-									echo '<td><input id="noGuest" type="number" name="children" placeholder="No. of Childrens" required min="0" value="0"></td>';
+									echo '<td><input id="noGuest" type="number" name="children" placeholder="No. of Childrens" required min="0" required></td>';
 								}
 								?>
 							</tr>
