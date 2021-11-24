716f93c8261aaa708eb87ca6f89bdaba80b56aaa
joshuamilante <milante.joshuajazon@ue.edu.ph>
2021-11-25 00:14:46 +0800

    Update Customer-Home.php

--- customer/Customer-Home.php
+++ customer/Customer-Home.php
@@ -304,7 +304,7 @@ $followingdata = $result->fetch_array(MYSQLI_ASSOC);
         <div class="container">
             <div class="row">
                 <div class="col-md-10 col-lg-8 mx-auto text-center">
-                <div class="mapouter"><div class="gmap_canvas"><iframe id="gmap_canvas" src="https://maps.google.com/maps?q=Luneta&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://fmovies-online.net">fmovies</a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:100px;}</style><a href="https://www.embedgooglemap.net">google maps embed zoom</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:100px;}</style></div></div>
+                <div class="mapouter"><div class="gmap_canvas"><iframe id="gmap_canvas" src="https://maps.google.com/maps?q=Luneta&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://fmovies-online.net">fmovies</a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:100%;}</style><a href="https://www.embedgooglemap.net">google maps embed zoom</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:100%;}</style></div></div>
                 </div>
             </div>
         </div>
