879b9c699da05f6907583e3f8cc19850e8d759ae
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-24 15:09:36 +0800

    dev: remove email sending on paypalsuccess page

--- customer/paypalSuccess.php
+++ customer/paypalSuccess.php
@@ -4,57 +4,14 @@ include('db.php');
 #$maxIDRes = mysqli_query($conn, $maxIDQ);
 #$maxIDRow = mysqli_fetch_assoc($maxIDRes);
 #$customerEmail = $maxIDRow['maxID'];
-(!isset($_POST['inp-email'])) && header("Location: /");
-$customerEmail = $_POST['inp-email'];
-$cID = intval(mysqli_real_escape_string($conn, $_POST['inp-cid']));
-
-
-// This variable ($bookingID) is required for the template
-$result = mysqli_fetch_all(mysqli_query($conn, "SELECT `checkInDate`, `fname`, `lname`, `reservationID` FROM `reservation` A INNER JOIN `customer` B ON A.`customerID`=B.`customerID` WHERE A.`customerID`=$cID LIMIT 1;"), MYSQLI_ASSOC)[0];
-$name = $result['fname']." ".$result['lname'];
-$bookingID = $result['reservationID'];
-$checkInDate = $result['checkInDate'];
-$checkInDate = date("F j, Y", strtotime($checkInDate)); // formats the checkin date
-
-ob_start();
-include "email-template.php";
-$template = ob_get_contents();  
-ob_end_clean();
-
-unset($name, $bookingID);
 
 ini_set( 'display_errors', 1 );
 error_reporting( E_ALL );
-$apiKey = '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';
-    use PHPMailer\PHPMailer\PHPMailer;
-    require 'vendor/autoload.php';
-    $mail = new PHPMailer;
-    $mail->isSMTP();
-    $mail->SMTPDebug = 0;
-    $mail->Host = 'smtp.sendgrid.net';
-    $mail->Port = 587;
-    $mail->SMTPAuth = true;
-    $mail->Username = 'apikey';
-    $mail->Password = $apiKey;
-    $mail->setFrom('thanoshotelreservation@ghrbs.site', 'Thanos');
-    $mail->addReplyTo('thanoshotelreservation@ghrbs.site', 'Thanos');
-    $mail->addAddress($customerEmail, 'Valued Guest');
-    $mail->Subject = 'GHRBS booking details';
-    $mail->Body = $template;
-    $mail->IsHTML(true); 
-    $mail->AltBody = "Booking ID: bookingID\n\nWe look forward to welcoming you to our resort on checkinDate.\n\nOur professional and friendly staff are committed to ensuring your stay is both enjoyable and comfortable.\n\nShould you have any requests prior to your stay, please do not hesitate to contact us at thanoshotelreservation@ghrbs.site  and we will endeavor to assist you whenever possible.\n\nThanks & Best Regards,\n\n
-    GHRBS team";
-    //$mail->addAttachment('test.txt');
-    if (!$mail->send()) {
-        //echo 'Mailer Error: ' . $mail->ErrorInfo;
-    } else {
-        //echo 'The email message was sent.';
-    }
+
 
 $query = "SELECT companyName FROM companyinfo";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 $followingdata = $result->fetch_array(MYSQLI_ASSOC);
-
 ?>
 
 
