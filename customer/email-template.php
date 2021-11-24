ce7c492d5ef4f819bd89396c5b2a7254234731a5
Xylo10 <benjbenito10@gmail.com>
2021-11-25 00:33:38 +0800

    Revert "Update email-template.php"
    This reverts commit fe8cc9d2470667a8d78e20131b33214945696d83.

--- customer/email-template.php
+++ customer/email-template.php
@@ -118,7 +118,8 @@
 																		style="font-size: 12px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; mso-line-height-alt: 14.399999999999999px; color: #FFFFFF; line-height: 1.2;">
 																		<p
 																			style="margin: 0; font-size: 18px; text-align: center;">
-																			<span style="font-size:24px;">Your Booking Confirmations:&nbsp;</span><br></p>
+																			<span style="font-size:24px;">Your booking
+																				confirmation!&nbsp;</span><br></p>
 																	</div>
 																</div>
 															</td>
@@ -157,16 +158,16 @@
 																		style="font-size: 12px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; mso-line-height-alt: 18px; color: #283C4B; line-height: 1.5;">
 																		<p
 																			style="margin: 0; font-size: 12px; mso-line-height-alt: 24px;">
-																			<span style="font-size:16px;"><span
+																			<span style="font-size:16px;"><strong><span
 																						style="font-size:16px;">Dear
-																						<strong><?php print  $name;?></span></strong></span>
+																						<?php print  $name;?></span></strong></span>
 																		</p>
 																		<p
 																			style="margin: 0; font-size: 12px; mso-line-height-alt: 24px;">
-																			<span style="font-size:16px;"><span
+																			<span style="font-size:16px;"><strong><span
 																						style="font-size:16px;">Thank
 																						you for choosing to stay with
-																						GHRBS.</span></span>
+																						GHRBS.</span></strong></span>
 																		</p>
 																	</div>
 																</div>
@@ -188,7 +189,10 @@
 																		<p
 																			style="margin: 0; font-size: 12px; mso-line-height-alt: 21px;">
 																			<span style="font-size:14px;">Booking ID:
-																				<?php print $bookingID ?? "Invalid booking id. Please contact us."; ?></span></p>
+																				<?php print $bookingID ?? "Invalid booking id. Please contact us."; ?></span></p><p
+																				style="margin: 0; font-size: 12px; mso-line-height-alt: 21px;">
+																				<span style="font-size:14px;">	Transaction ID:
+																					<?php print $paypalOrderID; ?></span></p>
 																		<p
 																			style="margin: 0; font-size: 12px; mso-line-height-alt: 21px;">
 																			<span style="font-size:14px;">We look
@@ -205,6 +209,34 @@
 																			style="margin: 0; font-size: 12px; text-align: left; mso-line-height-alt: 21px;">
 																			<span style="font-size:14px;">&nbsp;</span>
 																		</p>
+																		<p style="margin: 0; font-size: 12px; text-align: left; mso-line-height-alt: 21px;">
+																			<span style="font-size:14px;">Reservation Information</span></p>
+																		<div align:right;="">
+																			<table class="tg">
+																					
+																					<tbody>
+																					
+																					<tr>
+																						<td class="tg-zv4m">Check-in Date</td>
+																						<td class="tg-zv4m"><?php print $checkInDate; ?></td>
+																					</tr>
+																					<tr>
+																						<td class="tg-zv4m">Check-Out Date</td>
+																						<td class="tg-zv4m"><?php print $checkoutDate; ?></td>
+																					</tr>
+																					<tr>
+																						<td class="tg-zv4m">Guest:</td>
+																						<td class="tg-zv4m"><?php print $guest?></td>
+																					</tr>
+																					
+																					
+																					<tr>
+																						<td class="tg-zv4m"></td>
+																						<td class="tg-zv4m"></td>
+																					</tr>
+																					</tbody>
+																					</table><br>
+																			</div>
 																		<p
 																			style="margin: 0; font-size: 12px; text-align: left; mso-line-height-alt: 21px;">
 																			<span style="font-size:14px;">Billing information:</span></p>
@@ -219,27 +251,27 @@
 																					<tbody>
 																					<tr>
 																						<td class="tg-zv4m">Room rate: </td>
-																						<td class="tg-zv4m"></td>
+																						<td class="tg-zv4m"><?php print $roomRate; ?></td>
 																					</tr>
 																					<tr>
 																						<td class="tg-zv4m">VAT (12%): </td>
-																						<td class="tg-zv4m"><!--insert VAT here--></td>
+																						<td class="tg-zv4m"><?php print $vat; ?></td>
 																					</tr>
 																					<tr>
 																						<td class="tg-zv4m">Service Charge:</td>
-																						<td class="tg-zv4m"><!--insert Service charge here--></td>
+																						<td class="tg-zv4m"><?php print $serviceCharge; ?></td>
 																					</tr>
 																					<tr>
 																						<td class="tg-zv4m">Voucher Discount: </td>
-																						<td class="tg-zv4m"><!--insert voucher here--></td>
+																						<td class="tg-zv4m"><?php print $voucher_value; ?></td>
 																					</tr>
 																					<tr>
 																						<td class="tg-zv4m">Senior Citizen/PWD Discount: </td>
-																						<td class="tg-zv4m"><!--insert Senior/PWD discount here--></td>
+																						<td class="tg-zv4m"><?php print $PoS_value; ?></td>
 																					</tr>
 																					<tr>
 																						<td class="tg-zv4m"><b>Total:</b></td>
-																						<td class="tg-zv4m"><b><!--insert total here--></b></td>
+																						<td class="tg-zv4m"><b><?php print $total." ".$currency; ?></b></td>
 																					</tr>
 																					<tr>
 																						<td class="tg-zv4m"></td>
@@ -601,7 +633,6 @@
 																	cellpadding="0" cellspacing="0" role="presentation"
 																	align="right"
 																	style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
-																	
 																	<tr>
 																		<td style="padding:0 0 0 20px;"><a
 																				href="https://www.facebook.com/milantiyanih"
