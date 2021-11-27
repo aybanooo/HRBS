5b0e300021abcc71fe205479e5b44a02a03f23a1
Xylo10 <benjbenito10@gmail.com>
2021-11-28 03:14:44 +0800

    Update salesReport.php

--- customer/salesReport.php
+++ customer/salesReport.php
@@ -33,12 +33,12 @@ $sheet->setCellValue('F1', 'Revenue');
 $rowCount = 2;
 foreach ($data as $data1)
 {
-    $sheet->setCellValue('A'. $rowCount, $data1['$reservationID']);
-    $sheet->setCellValue('B'. $rowCount, $data1['$customerID']);
-    $sheet->setCellValue('C'. $rowCount, $data1['numberOfNightstay']);
-    $sheet->setCellValue('D'. $rowCount, $data1['payedValue']);
-    $sheet->setCellValue('E'. $rowCount, $data1['origRoomRate']);
-    $sheet->setCellValue('F'. $rowCount, $data1['payedvalue'] * $data1['numberOfNightstay']);
+    $sheet->setCellValue('A'. $rowCount, $data1["reservationID"]);
+    $sheet->setCellValue('B'. $rowCount, $data1["customerID"]);
+    $sheet->setCellValue('C'. $rowCount, $data1["numberOfNightstay"]);
+    $sheet->setCellValue('D'. $rowCount, $data1["payedValue"]);
+    $sheet->setCellValue('E'. $rowCount, $data1["origRoomRate"]);
+    $sheet->setCellValue('F'. $rowCount, $data1["payedValue"] * $data1['numberOfNightstay']);
     $rowCount++;
 }
 
