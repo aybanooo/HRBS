c858b30d6a828cafb4c882f39439e497d53118b9
aybanoooUE <delossantos.johnmarkivan@ue.edu.ph>
2021-11-27 21:42:09 +0800

    dev: update pattern message

--- customer/Customer-Booking_Form.php
+++ customer/Customer-Booking_Form.php
@@ -502,7 +502,7 @@ if(isset($_GET['d']) && $_GET['d'] != "") {
 								<td>
 									<div id="seniorDiv">
 										<div class="form-group">
-											<input id="discount" name="discount" type="text" placeholder="ID Number" autocomplete="off" pattern="^[\w|\d]+(-[\w|\d]+)*$" tle="Must contain letters/numbers/-(dash)">
+											<input id="discount" name="discount" type="text" placeholder="ID Number" autocomplete="off" pattern="^[\w|\d]+(-[\w|\d]+)*$" title="Must only contain letters / numbers / - (dash)">
 										</div>
 									</div>
 								</td>
